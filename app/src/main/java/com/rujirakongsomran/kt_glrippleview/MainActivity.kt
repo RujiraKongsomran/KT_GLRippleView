1400ade333085a9be6b9b0af959da8f0305c166d
Rujira Kongsomran <biwberry@outlook.com>
2020-06-04 23:11:04 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/kt_glrippleview/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_glrippleview/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.rujirakongsomran.kt_glrippleview
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        glView.run {
+            addBackgroundImages(listOf(
+                BitmapFactory.decodeResource(resources, R.drawable.chocolate_cake),
+                BitmapFactory.decodeResource(resources, R.drawable.donut)
+            ))
+            setRippleOffset(0.01f)
+            setFadeInterval(2000)
+            startCrossFadeAnimation()
+        }
+
     }
 }
